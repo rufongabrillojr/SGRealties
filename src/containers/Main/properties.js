acf7012205a589306e2ad0f16c16ca6138072f49
rufongabrillojr <rufo.gabrillo@bliimo.com>
2020-12-26 02:00:41 +0800

    Clean ui and widen the product information

--- src/containers/Main/properties.js
+++ src/containers/Main/properties.js
@@ -79,5 +79,77 @@ export default list = [
       lat: 16.5183969,
       lng: 120.5305563,
       image: defaultPropsImage6
+    },
+    {
+      id: 0,
+      name: 'Lorem ipsum dolor sit amet 1',
+      price: {
+        min: '2M',
+        max: '2.5M'
+      },
+      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      lat: 15.9603307,
+      lng: 120.3856262,
+      image: defaultPropsImage1
+    },
+    {
+      id: 1,
+      name: 'Lorem ipsum dolor sit amet 2',
+      price: {
+        min: '1M',
+        max: '1.5M'
+      },
+      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      lat: 15.927525,
+      lng: 120.3441483,
+      image: defaultPropsImage2
+    },
+    {
+      id: 2,
+      name: 'Lorem ipsum dolor sit amet 3',
+      price: {
+        min: '1.2M',
+        max: '1.3M'
+      },
+      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      lat: 15.782365,
+      lng: 121.015441,
+      image: defaultPropsImage3
+    },
+    {
+      id: 3,
+      name: 'Lorem ipsum dolor sit amet 4',
+      price: {
+        min: '500K',
+        max: '600K'
+      },
+      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      lat: 14.641009,
+      lng: 121.001285,
+      image: defaultPropsImage4
+    },
+    {
+      id: 4,
+      name: 'Lorem ipsum dolor sit amet 5',
+      price: {
+        min: '100K',
+        max: ''
+      },
+      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      lat: 16.4183969,
+      lng: 120.6305563,
+      image: defaultPropsImage5
+    },
+    {
+      id: 5,
+      name: 'Lorem ipsum dolor sit amet 6',
+      price: {
+        min: '1000000',
+        max: ''
+      },
+      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      lat: 16.5183969,
+      lng: 120.5305563,
+      image: defaultPropsImage6
     }
   ]
